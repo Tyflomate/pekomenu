92f8a4ec39a86f580368433154bd0e029c778d7a
Tyflomate <florian@tymate.com>
2021-01-12 17:29:02 +0100

    feat: use of ReverseMarkdown gem to translate html to markdown

--- pekomenu.rb
+++ pekomenu.rb
@@ -1,18 +1,9 @@
 require 'slack-ruby-bot'
 require 'open-uri'
 require 'nokogiri'
+require 'reverse_markdown'
 
-# le menu est dans l'ul featureddish
 site = Nokogiri::HTML.parse(URI.open('https://www.pekopeko.fr/food-truck/'))
-
 menu_lines = site.xpath("//ul[@class=\"featureddish\"]/li")
-res = menu_lines.each do |line|
-  dish_name = "# #{line.at_css("h6").content} #"
-  dish_description = line.at_css("div.menu-description").content
-  dish_price = line.at_css("div.price") != nil ? line.at_css("div.price").content : " Pas de prix annoncé"
-  dish_image = line.at_css("a.pretty_image") != nil ? "![alt text](#{line.at_css("a.pretty_image").attributes["href"].content} \"Dish\")" : ""
 
-  puts dish_name << "\n\n" << dish_description << "\n\n" << dish_price << "\n\n" << dish_image << "\n\n"
-end
-
-res
+puts ReverseMarkdown.convert(menu_lines)
