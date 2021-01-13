00d2c0e9f77b5590524ae0c23b5949aa86d6daf2
Tyflomate <florian@tymate.com>
2021-01-13 18:08:44 +0100

    feat: send message every two minutes with cron & github actions

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'celluloid-io'
 gem 'dotenv'
-gem 'nokogiri', ">= 1.11.0.rc4"
+gem 'nokogiri', '>= 1.11.0.rc4'
 gem 'puma'
 gem 'reverse_markdown'
-gem 'slack-ruby-bot'
+gem 'slack-ruby-client'
--- pekomenu.rb
+++ pekomenu.rb
@@ -1,9 +1,19 @@
-require 'slack-ruby-bot'
+# frozen_string_literal: true
+
+require 'slack-ruby-client'
 require 'open-uri'
 require 'nokogiri'
 require 'reverse_markdown'
+require 'dotenv'
+
+Dotenv.load
+
+client = Slack::Web::Client.new(token: ENV['SLACK_API_TOKEN'])
+client.auth_test
 
 site = Nokogiri::HTML.parse(URI.open('https://www.pekopeko.fr/food-truck/'))
-menu_lines = site.xpath("//ul[@class=\"featureddish\"]/li")
+menu_lines = site.xpath('//ul[@class="featureddish"]/li')
 
-puts ReverseMarkdown.convert(menu_lines)
+bot_message = ReverseMarkdown.convert(menu_lines)
+
+client.chat_postMessage(channel: '#testbot', text: bot_message, as_user: true)
