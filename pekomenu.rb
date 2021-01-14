397e4caf66cac526bc056a6111d4eec49fa9a536
Tao <tao@tymate.com>
2021-01-14 16:34:47 +0100

    chore: review style

--- pekomenu.rb
+++ pekomenu.rb
@@ -11,19 +11,18 @@ require_relative 'dish'
 Dotenv.load
 
 client = Slack::Web::Client.new(token: ENV['SLACK_API_TOKEN'])
-client.auth_test
 
 site = Nokogiri::HTML.parse(URI.open('https://www.pekopeko.fr/food-truck/'))
 menu_lines = site.xpath('//ul[@class="featureddish"]/li')
 
 res = []
 menu_lines.each do |line|
-  dish_name = line.at_css('h6').content
-  dish_description = line.at_css('div.menu-description').content
-  dish_price = !line.at_css('div.price').nil? ? line.at_css('div.price').content : 'Pas de prix annoncé'
-  dish_image = !line.at_css('a.pretty_image').nil? ? line.at_css('a.pretty_image').attributes['href'].content : ''
+  name = line.at_css('h6').content
+  description = line.at_css('div.menu-description').content
+  price = line.at_css('div.price').nil? ? 'Pas de prix annoncé' : line.at_css('div.price').content
+  image_url = line.at_css('a.pretty_image').nil? ? '' : line.at_css('a.pretty_image').attributes['href'].content
 
-  res.append(Dish.new(dish_name, dish_description, price: dish_price, image_url: dish_image))
+  res.append(Dish.new(name: name, description: description, price: price, image_url: image_url))
 end
 
 client.chat_postMessage(channel: '#random', blocks: res, as_user: true)
