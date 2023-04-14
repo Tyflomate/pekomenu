d3d1b637fe244f1958da43bfd2360b340062b74a
Henri Michel <tip@me.com>
2023-04-14 07:52:41 +0200

    feat: update document parsing

--- pekomenu.rb
+++ pekomenu.rb
@@ -13,14 +13,14 @@ Dotenv.load
 client = Slack::Web::Client.new(token: ENV['SLACK_API_TOKEN'])
 
 site = Nokogiri::HTML.parse(URI.open('https://www.pekopeko.fr/camion/'))
-menu_lines = site.xpath('//ul[@class="featureddish"]/li')
+menu_lines = site.css('.et_pb_with_background .et_pb_row .et_pb_blurb_content')
 
 res = []
 menu_lines.each do |line|
-  name = line.at_css('h6').content
-  description = line.at_css('div.menu-description').content
-  price = line.at_css('div.price')&.content || 'Pas de prix annoncé'
-  image_url = line.at_css('a.pretty_image')&.attributes&.dig('href')&.content || ''
+  name, price = line.at_css('h4 span').content.split(" | ")
+  price = price || 'Pas de prix annoncé'
+  description = line.at_css('et_pb_blurb_description').content || ''
+  image_url = line.at_css('img')&.attributes&.dig('src').content || ''
 
   res.append(Dish.new(name: name, description: description, price: price, image_url: image_url))
 end
