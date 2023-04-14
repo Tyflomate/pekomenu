8d0d59c40fc6d7b4680eb0639d10f046ba11e641
Henri Michel <tip@me.com>
2023-04-14 07:54:20 +0200

    fix: fix CSS class parsing

--- pekomenu.rb
+++ pekomenu.rb
@@ -19,7 +19,7 @@ res = []
 menu_lines.each do |line|
   name, price = line.at_css('h4 span').content.split(" | ")
   price = price || 'Pas de prix annoncé'
-  description = line.at_css('et_pb_blurb_description').content || ''
+  description = line.at_css('.et_pb_blurb_description').content || ''
   image_url = line.at_css('img')&.attributes&.dig('src').content || ''
 
   res.append(Dish.new(name: name, description: description, price: price, image_url: image_url))
